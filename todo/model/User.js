accb1fd835d8cafd60ff24b42fbc4c6967b76a7a
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-09-29 10:12:15 +0330

    npm  i mongoose & Create Model Schema

---
+++ todo/model/User.js
@@ -0,0 +1,23 @@
+import { Schema,model,models } from "mongoose";
+
+const userSchema = new Schema({
+    email :{
+        type :String ,
+        required : true
+    },
+    password :{
+        type : String ,
+        required : true ,
+    },
+    name : String ,
+    lastName : String,
+    createAt :{
+        type : Date,
+        Date : ()=> Date.now(),
+        immutable : true,
+    },
+    todos :[{title : String , status : String}]
+});
+
+const User = models.User || model("User" , userSchema);
+export default User;
