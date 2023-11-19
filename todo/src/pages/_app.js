6dff50be4787c0f234b53d37143fe65b788085cc
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-20 00:07:02 +0330

    checked some function

--- todo/src/pages/_app.js
+++ todo/src/pages/_app.js
@@ -1,11 +1,15 @@
-import { SessionProvider } from 'next-auth/react'
-import Layout from '../../components/layout/Layout'
+import { SessionProvider } from "next-auth/react";
+import Layout from "../../components/layout/Layout";
 
-
-
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <SessionProvider session={pageProps.session}>
-           <Layout> <Component {...pageProps} /> </Layout>
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        {" "}
+        <Component {...pageProps} />{" "}
+      </Layout>
     </SessionProvider>
+  );
 }
