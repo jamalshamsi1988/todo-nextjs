c78420558d1e9651b300b79304f65a141319467d
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-22 00:43:52 +0330

    handel the function for next and back

--- todo/components/module/Tasks.js
+++ todo/components/module/Tasks.js
@@ -2,6 +2,15 @@ import {RiMastodonLine} from 'react-icons/ri';
 import {BiRightArrow,BiLeftArrow} from 'react-icons/bi';
 
 const Tasks = ({data,next,back,fetchTodos}) => {
+  const changeHandler=async (id,status)=>{
+    const res=await fetch("/api/todos",{
+      method:"PATCH",
+      body:JSON.strinify({id , status}),
+      headers :{"Content-Type" : "application/json"}
+    });
+    const data = await res.json();
+    if(data.status === "success") fetchTodos();
+  }
   return (
     <div className="tasks">
         {
@@ -10,8 +19,8 @@ const Tasks = ({data,next,back,fetchTodos}) => {
                 <RiMastodonLine />
                 <h4>{item.title}</h4>
                 <div>
-                  {back ? <button className="button-back"><BiLeftArrow/> Back</button> : null}
-                  {next ? <button className="button-next">Next<BiRightArrow/> </button> : null}
+                  {back ? <button className="button-back" onClick={()=> changeHandler(i._id , back)}><BiLeftArrow/> Back</button> : null}
+                  {next ? <button className="button-next"onClick={()=> changeHandler(i._id , next)}>Next<BiRightArrow/> </button> : null}
                   </div>
             </div>)
         }
