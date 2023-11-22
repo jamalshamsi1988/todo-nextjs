5dd0cc77f418633eb32d51e58351deff7e7251fc
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-22 16:47:43 +0330

    mange show info of user

--- todo/components/template/ProfilePage.js
+++ todo/components/template/ProfilePage.js
@@ -1,31 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import ProfileForm from "../module/ProfileForm";
+import ProfileData from "../module/ProfileData";
 
 const ProfilePage = () => {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-  const submitHandler= async()=>{
-    const res= await fetch("/api/profile",{
-        method :"POST",
-        body:JSON.stringify({name,lastName,password}),
-        headers :{"Content-Type" :"application/json"}
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  const fetchProfile = async () => {
+    const res = await fetch("/api/profile");
+    const data = await res.json();
+    if (data.status === "success" && data.data.name && data.data.lastName) {
+      setData(data.data);
+    }
+  };
+  const submitHandler = async () => {
+    const res = await fetch("/api/profile", {
+      method: "POST",
+      body: JSON.stringify({ name, lastName, password }),
+      headers: { "Content-Type": "application/json" },
     });
-    const data=await res.json();
-  }
+    const data = await res.json();
+    console.log(data)
+  };
   return (
     <div className="profile-form">
       <h2>Profile</h2>
-      <ProfileForm
-        name={name}
-        lastName={lastName}
-        password={password}
-        setName={setName}
-        setLastName={setLastName}
-        setPassword={setPassword}
-        submitHandler={submitHandler}
-      />
+      {data ? (
+        <ProfileData data={data} />
+      ) : (
+        <ProfileForm
+          name={name}
+          lastName={lastName}
+          password={password}
+          setName={setName}
+          setLastName={setLastName}
+          setPassword={setPassword}
+          submitHandler={submitHandler}
+        />
+      )}
     </div>
   );
 };
